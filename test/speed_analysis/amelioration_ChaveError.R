e6c792edf6511ec0a1d8c8f921930c31301a6dfb
Arthur Péré <arthur.pere@free.fr>
2018-09-06 14:38:45 +0200

    rewrite the file for the reading

--- test/speed_analysis/amelioration_ChaveError.R
+++ test/speed_analysis/amelioration_ChaveError.R
@@ -1,6 +1,30 @@
 library(microbenchmark)
 
-rt = function(D_simu){
+myrtruncnorm <- function(n,lower = -1, upper = 1,mean=0,sd=1) {
+  qnorm(runif(n,pnorm(lower,mean=mean,sd=sd),pnorm(upper,mean=mean,sd=sd)),mean=mean,sd=sd)
+}
+
+
+
+
+
+rt_msm = function(D_simu){
+  fivePercent <- round( nrow(D_simu) * 5 / 100 )
+  chaveError <- function(x)
+  { 
+    ## Assigning large errors on 5% of the trees
+    largeErrSample <- sample(length(x), fivePercent)
+    x[largeErrSample] <- myrtruncnorm(n = fivePercent, mean = x[largeErrSample], sd = 4.64, lower = 0.1, upper = 500)
+    ## Assigning small errors on the remaining 95% trees
+    D_sd <- 0.0062 * x[-largeErrSample] + 0.0904
+    x[-largeErrSample] <- msm::rtnorm(n =length(D_sd), mean = x[-largeErrSample], sd = D_sd, lower = 0.1, upper = 500)
+    return(x)
+  }
+  D_simu <- apply(D_simu, 2, chaveError)
+  return(D_simu)
+}
+
+rt_myrtroncnorm = function(D_simu){
   fivePercent <- round( nrow(D_simu) * 5 / 100 )
   chaveError <- function(x)
   { 
@@ -16,17 +40,17 @@ rt = function(D_simu){
   return(D_simu)
 }
 
-rt1 = function(D_simu){
+
+rt_improvement = function(D_simu){
   fivePercent <- round( nrow(D_simu) * 5 / 100 )
   chaveError <- function(x)
   { 
     ## Assigning large errors on 5% of the trees
     largeErrSample <- sample(length(x), fivePercent)
     
-    D_sd = 0.0062 * x + 0.0904
+    D_sd = 0.0062 * x + 0.0904 # Assigning small errors on the remaining 95% trees
     D_sd[largeErrSample] = 4.64
     
-
     x <- myrtruncnorm(n =length(x), mean = x, sd = D_sd, lower = 0.1, upper = 500)
     return(x)
   }
@@ -34,7 +58,7 @@ rt1 = function(D_simu){
   return(D_simu)
 }
 
-result = microbenchmark("rt" = rt(D_simu), "rt1" = rt1(D_simu), times = 10)
+result = microbenchmark("rt_msm" = rt_msm(D_simu), "rt_myr" = rt_myrtroncnorm(D_simu), "rt_improvement"=rt_improvement(D_simu), times = 10)
 plot(result)
 
 
@@ -61,8 +85,8 @@ myrtruncnorm <- function(n,lower = -1, upper = 1,mean=0,sd=1) {
 }
 
 
-microbenchmark("rtnorm" = rtnorm(10^5, lower = -1, upper = 1), 
+microbenchmark("rtnorm" = msm::rtnorm(10^5, lower = -1, upper = 1), 
                     "tnorm"= tnorm(10^5), 
                     "mytruncnorm"=myrtruncnorm(10^5, a=-1, b=1),
-                    "rtruncnorm" = rtruncnorm(10^5, a=-1, b=1))
+                    "rtruncnorm" = truncnorm::rtruncnorm(10^5, a=-1, b=1))
 
